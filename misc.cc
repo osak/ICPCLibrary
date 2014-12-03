3e2da81af52a6a188b54dd9a4266e31a2a3ee3ef
Koga Osamu <osak.63@gmail.com>
2014-12-03 22:00:25 +0900

    Maybe

--- misc.cc
+++ misc.cc
@@ -369,3 +369,18 @@ int find(const string &src, const string &str, const vector<int> &sa) {
     }
     return -1;
 }/*}}}*/
+
+template <typename T>
+struct Maybe {/*{{{*/
+    T val;
+    bool valid;
+
+    Maybe() : valid(false) {}
+    Maybe(T &t) : val(t), valid(true) {}
+
+    T& operator =(const T &rv) {
+        val = rv;
+        valid = true;
+        return val;
+    }
+};/*}}}*/
